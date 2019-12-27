4a1cb61fdb7cb477c9d2f5e57a4541a480fa0bcc
unknown <np03a170023@heraldcollege.edu.np>
2019-12-27 17:00:55 +0545

    MPI

---
+++ passwordcracking/pwdmpi.c
@@ -0,0 +1,156 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <crypt.h>
+#include <mpi.h>
+
+/*****************************************************************************
+The variable names and the function names of this program is same as provided by the university.
+The added variable and function are the only changes made to this program.
+  
+
+To compile:
+     mpicc -o passwordmpi pwdmpi.c -lrt -lcrypt
+     
+  To run 3 processes on this computer:
+    mpirun -n 3 ./passwordmpi >outcome.txt
+*****************************************************************************/
+
+
+int n_passwords = 4;
+pthread_t t1, t2;
+char *encrypted_passwords[] = {
+  "$6$KB$yPvAP5BWyF7oqOITVloN9mCAVdel.65miUZrEel72LcJy2KQDuYE6xccHS2ycoxqXDzW.lvbtDU5HuZ733K0X0",
+  "$6$KB$G/Nlf9jJdkbjMHMqOD0scV20t6cnhtojQKz8v14fhG26GmKOiz.Bd4QXDLeh9SjiEqsBozC812Yf5bvbFWz131",
+  "$6$KB$ciaKBZs/3LSZlaDE.dagGO0ZE9YfYymWUpzFnIuDTUviDxpqsUVE0HE1/HUXgMp0xl6IfVEzDljEW1iy6ttlG.",
+  "$6$KB$DY2hmqaRHB3ILUMAMW9k0b.1iHkAfzAe5RtI/stVutttQ.IIV3GcFyQzepzzQhlyGr5nPxIchG6Im8/hYNhzj1"
+};
+
+void substr(char *dest, char *src, int start, int length){
+  memcpy(dest, src + start, length);
+  *(dest + length) = '\0';
+}
+  
+void kernel_function1(char *salt_and_encrypted){
+  int s, a, m;     
+  char salt[7];    
+
+  char plain[7];   
+  char *enc;       
+  int count = 0;   
+
+  substr(salt, salt_and_encrypted, 0, 6);
+  
+  for(s='A'; s<='M'; s++){
+    for(a='A'; a<='m'; a++){
+      for(m=0; m<=99; m++){
+	//printf("Instance 1:");
+	sprintf(plain, "%c%c%02d",s, a, m);
+	enc = (char *) crypt(plain, salt);
+	count++;
+	if(strcmp(salt_and_encrypted, enc) == 0){
+	  printf("#%-8d%s %s\n", count, plain, enc);
+	} else {
+	  printf(" %-8d%s %s\n", count, plain, enc);
+	}
+      }
+    }
+  }
+  printf("%d solutions explored\n", count);
+}
+void kernel_function2(char *salt_and_encrypted){
+  int s, a, m;     
+  char salt[7];    
+
+  char plain[7];   
+  char *enc;       
+  int count = 0;  
+
+  substr(salt, salt_and_encrypted, 0, 6);
+  
+  for(s='N'; s<='m'; s++){
+    for(a='A'; a<='m'; a++){
+      for(m=0; m<=99; m++){
+	//printf("Instance 2:");
+	sprintf(plain, "%c%c%02d",s, a, m);
+	enc = (char *) crypt(plain, salt);
+	count++;
+	if(strcmp(salt_and_encrypted, enc) == 0){
+	  printf("#%-8d%s %s\n", count, plain, enc);
+	} else {
+	  printf(" %-8d%s %s\n", count, plain, enc);
+	}
+      }
+    }
+  }
+  printf("%d solutions explored\n", count);
+}
+
+
+int time_difference(struct timespec *start, struct timespec *finish,
+                    long long int *difference) {
+  long long int ds =  finish->tv_sec - start->tv_sec; 
+  long long int dn =  finish->tv_nsec - start->tv_nsec; 
+
+  if(dn < 0 ) {
+    ds--;
+    dn += 1000000000; 
+  } 
+  *difference = ds * 1000000000 + dn;
+  return !(*difference > 0);
+}
+
+int main(int argc, char** argv) {
+ struct timespec start, finish;   
+  long long int time_elapsed;
+
+  clock_gettime(CLOCK_MONOTONIC, &start);
+
+ 
+  int size, rank;
+int i;
+
+  MPI_Init(NULL, NULL);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if(size != 3) {
+    if(rank == 0) {
+      printf("This program needs to run on exactly 3 processes\n");
+    }
+  } else {
+    if(rank ==0){
+
+      int s;
+    
+      MPI_Send(&s, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+      MPI_Send(&s, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
+
+
+ 
+    } else if(rank==1){
+      int number;
+      MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, 
+                         MPI_STATUS_IGNORE);
+        for(i=0;i<n_passwords;i<i++) {
+          kernel_function1(encrypted_passwords[i]);
+        }
+      }
+      else{
+      int number;
+      MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, 
+                         MPI_STATUS_IGNORE);
+        for(i=0;i<n_passwords;i<i++) {
+          kernel_function2(encrypted_passwords[i]);
+        }
+      }
+    }
+    MPI_Finalize(); 
+ clock_gettime(CLOCK_MONOTONIC, &finish);
+  time_difference(&start, &finish, &time_elapsed);
+  printf("Time elapsed was %lldns or %0.9lfs\n", time_elapsed,
+         (time_elapsed/1.0e9)); 
+
+  return 0;
+}
